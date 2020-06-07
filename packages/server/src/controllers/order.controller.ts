112fb83b813df5b0d3ffa0aca3c064c615fc4a4e
keyner07 <skerling19@hotmail.com>
2020-06-07 17:47:23 -0400

    feat: adding the function getOrder

--- packages/server/src/controllers/order.controller.ts
+++ packages/server/src/controllers/order.controller.ts
@@ -9,3 +9,18 @@ export async function getOrders(req: Request, res: Response): Promise<Response> 
         return res.status(500).json({ message: "Occur a problem with the server." });
     }
 }
+
+export async function getOrder( req: Request, res: Response): Promise<Response> {
+    try {
+        const idOrder: string = req.params.id;
+        const order = await Order.findById(idOrder);
+        if (order) {
+            return res.status(200).json(order);
+        }
+        else {
+            return res.status(404).json({ message: "Order not found." });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: "Occur a problem with the server." });
+    }
+}
