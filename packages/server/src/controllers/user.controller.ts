f19528a896b5c37631379d5def8309c6b6dce83f
keyner07 <skerling19@hotmail.com>
2020-06-07 14:53:22 -0400

    fix: adding try catch in the controllers

--- packages/server/src/controllers/user.controller.ts
+++ packages/server/src/controllers/user.controller.ts
@@ -4,34 +4,44 @@ import User, { IUser } from '../models/user.model';
 import { generateToken } from '../auth/index';
 
 export async function signUp( req: Request, res: Response): Promise<Response> {
-    const { name, email, password, isAdmin } = req.body;
-    if (!name || !email || !password || !isAdmin ) {
-        return res.status(400).json({ message: "Please. Send all your data." });
-    }
+    try {
+        const { name, email, password, isAdmin } = req.body;
+        if (!name || !email || !password || !isAdmin ) {
+            return res.status(400).json({ message: "Please. Send all your data." });
+        }
 
-    const user = await User.findOne({ email: email });
-    if (user) {
-        return res.status(400).json({ message: "The user already exists." });
+        const user = await User.findOne({ email: email });
+        if (user) {
+            return res.status(400).json({ message: "The user already exists." });
+        }
+        const newUser = new User(req.body);
+        await newUser.save();
+        return res.status(201).json(newUser);
+    } catch(err) {
+        return res.status(500).json({ message: "The server has a error." });
+        console.error(`[CSU] The problem is ${err}`);
     }
-    const newUser = new User(req.body);
-    await newUser.save();
-    return res.status(201).json(newUser);
 }
 
 export async function signIn( req: Request, res: Response): Promise<Response> {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(400).json({ message: "Please. Send your email and password." });
-    }
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-        return res.status(400).json({ message: "The user doesn't exists." });
-    }
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "Please. Send your email and password." });
+        }
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ message: "The user doesn't exists." });
+        }
 
-    const isMatch = await user.comparePassword(req.body.password);
-    if (isMatch) {
-        return res.status(400).json({ token: generateToken(user) });
-    }
+        const isMatch = await user.comparePassword(req.body.password);
+        if (isMatch) {
+            return res.status(400).json({ token: generateToken(user) });
+        }
 
-    return res.status(400).json({ message: "The email or password are incorrect." });
+        return res.status(400).json({ message: "The email or password are incorrect." });
+    } catch(err) {
+        return res.status(500).json({ message: "The server has a error." });
+        console.error(`[CSI] The problem is ${err}`);
+    }
 }
